118f0fa2da0fc35e5b469c98bc99b2c1b4ad23d2
Luca Paganin <luca.paganin1994@gmail.com>
2023-10-20 22:41:15 +0200

    testing

--- bot.py
+++ bot.py
@@ -1,88 +1,36 @@
-import telegram, os
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-from dictionary import get_info
+import httpx, logging
+import pandas as pd
+import json
+from pathlib import Path
+from datetime import datetime, timedelta
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-telegram_bot_token = "1969028284:AAF0oDjLtKoodTMiiWrIyTXkz8XqiJumy0w"
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-updater = Updater(token=telegram_bot_token, use_context=True)
-dispatcher = updater.dispatcher
+TOKEN = "1969028284:AAF0oDjLtKoodTMiiWrIyTXkz8XqiJumy0w"
+CACHE_DURATION = timedelta(minutes=5)
+GROUPID = -4080210648
 
+cache = {
+    "last_update": datetime.now() - CACHE_DURATION,
+    "data": None
+}
 
-# set up the introductory statement for the bot when the /start command is invoked
-def start(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello there. Provide any English word and I will give you a bunch "
-                                                   "of information about it.")
+async def aspi(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("stacca stacca")
 
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
 
-# obtain the information of the word provided and format before presenting.
-def get_word_info(update, context):
-    # get the word info
-    word_info = get_info(update.message.text)
+    app.add_handler(CommandHandler("aspi", aspi))
 
-    # If the user provides an invalid English word, return the custom response from get_info() and exit the function
-    if word_info.__class__ is str:
-        update.message.reply_text(word_info)
-        return
-
-    # get the word the user provided
-    word = word_info['word']
-
-    # get the origin of the word
-    origin = word_info['origin']
-    meanings = '\n'
-
-    synonyms = ''
-    definition = ''
-    example = ''
-    antonyms = ''
-
-    # a word may have several meanings. We'll use this counter to track each of the meanings provided from the response
-    meaning_counter = 1
-
-    for word_meaning in word_info['meanings']:
-        meanings += 'Meaning ' + str(meaning_counter) + ':\n'
-
-        for word_definition in word_meaning['definitions']:
-            # extract the each of the definitions of the word
-            definition = word_definition['definition']
-
-            # extract each example for the respective definition
-            if 'example' in word_definition:
-                example = word_definition['example']
-
-            # extract the collection of synonyms for the word based on the definition
-            for word_synonym in word_definition['synonyms']:
-                synonyms += word_synonym + ', '
-
-            # extract the antonyms of the word based on the definition
-            for word_antonym in word_definition['antonyms']:
-                antonyms += word_antonym + ', '
-
-        meanings += 'Definition: ' + definition + '\n\n'
-        meanings += 'Example: ' + example + '\n\n'
-        meanings += 'Synonym: ' + synonyms + '\n\n'
-        meanings += 'Antonym: ' + antonyms + '\n\n\n'
-
-        meaning_counter += 1
-
-    # format the data into a string
-    message = f"Word: {word}\n\nOrigin: {origin}\n{meanings}"
-
-    update.message.reply_text(message)
-
-# run the start function when the user invokes the /start command 
-dispatcher.add_handler(CommandHandler("start", start))
-
-# invoke the get_word_info function when the user sends a message 
-# that is not a command.
-dispatcher.add_handler(MessageHandler(Filters.text, get_word_info))
-
-updater.start_webhook(
-    listen='0.0.0.0',
-    port=443,
-    url_path='TOKEN',
-    # key='private.key',
-    # cert='cert.pem',
-    webhook_url=f'https://aspi-bot-387e664a1cf9.herokuapp.com/{telegram_bot_token}'
-) 
+    app.run_webhook(
+        listen='0.0.0.0',
+        port=443,
+        url_path=TOKEN,
+        webhook_url=f"https://aspi-bot-387e664a1cf9.herokuapp.com/{TOKEN}"
+    )
--- oldbot.py
+++ oldbot.py
@@ -95,4 +95,6 @@ if __name__ == '__main__':
 
     app.add_handler(CommandHandler("aspi", aspi))
 
-    app.run_polling()
+    app.run_webhook(
+        
+    )
